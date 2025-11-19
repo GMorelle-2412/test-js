7e850c26c3bc3cbd1435ed67e287b05e21c74946
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2025-11-19 11:11:14 +0100

    commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+//var permet de déclarer une variable globale
+var myvar = "Hello world";
+var monNombre = 42;
+
+//let permet de déclarer une variable
+let mavariable = "je suis locale";
+
+//const permet de déclarer une constent
+const Ma_constent = 3.14;
+
+alert("coucou tout le monde");
+
+//regarder le résultat sur inspecter et console
+console.log("bb");
+
+function mafonction(parametre){
+    console.log("coucou "+parametre+" !");
+}
+
+mafonction("Julien")
+mafonction("alice")
+mafonction("le bo")
